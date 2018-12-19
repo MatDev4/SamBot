6f0179a1d61a20ef068396947bff681b33166cdd
Ze_Dreams <34602094+Nicoxx45@users.noreply.github.com>
2018-12-19 19:08:29 +0100

    v0.9.7 NEW!

--- files/APIevents/ready.js
+++ files/APIevents/ready.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
     console.log(`[${client.user.tag}] Connected! ${client.guilds.size} guilds and ${client.users.size} users`)
     client.user.setStatus("online");
     setInterval(function() {
-    let RANDOMCHOOSER = ["s!help | v0.9.6", `s!help | ${client.guilds.size} guilds!`, `s!help | ${client.users.size} users!`, "s!help | OwO Thanks!"]
+    let RANDOMCHOOSER = ["s!help | v0.9.7", `s!help | ${client.guilds.size} guilds!`, `s!help | ${client.users.size} users!`, "s!help | OwO Thanks!"]
     let STATUSCHOOSER = Math.floor(Math.random()*(RANDOMCHOOSER.length));
         client.user.setActivity(RANDOMCHOOSER[STATUSCHOOSER]);
     }, 7000);
