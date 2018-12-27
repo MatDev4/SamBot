347a1c60aa62607febe390f99cba192eb028c3fb
Ze_Dreams <34602094+Nicoxx45@users.noreply.github.com>
2018-12-27 15:44:52 +0100

    Delete sam-rules command

--- files/dev/sam-test.js
+++ files/dev/sam-test.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   if (message.author.id !== "263713074630885376") return message.reply(i18n.t('NOP_Botowner'))
   if(message.author.bot) return;
   message.delete()
-// CMD
+ message.channel.send("...")
 }
 
 
