90cf89f0dfb177b6b8908320576fecc68a040eb2
Ze_Dreams <34602094+Nicoxx45@users.noreply.github.com>
2018-12-27 16:27:22 +0100

    Updated invite command

--- files/misc/invite.js
+++ files/misc/invite.js
@@ -16,11 +16,15 @@ module.exports.run = async (bot, message, args) => {
     if(message.author.bot) return;
     let orange = botconfig.orange
     let blue = botconfig.discordblue
-
     const InviteEmbed = new Discord.RichEmbed()
-    .setTitle("Invite SamBot")
+    .setTitle(client.user.tag)
+    .setDescription("Hey, thanks for inviting me to your Discord server! By inviting me, you encourage my developers and improve my visibility on Discord.")
+    .addField("**Basic invitation", "This invitation is the default when you invite me. It contains all the required permissions. [> Invite](https://discordapp.com/oauth2/authorize/?permissions=2146827639&scope=bot&client_id=481753714982518786)", true)
+    .addField("**Administrator**", "You can make me join your server with Administrator permission, this allows you not to need to change the Bot permissions as updates are made. [> Invite](https://discordapp.com/oauth2/authorize/?permissions=8&scope=bot&client_id=481753714982518786)", true)
+    .addField("**No permission**", "You want to invite me just to decorate? OK. [> Invite](https://discordapp.com/oauth2/authorize/?permissions=0&scope=bot&client_id=481753714982518786)", true)
+    .addField("**Configure permissions**", "This can be useful when you want to have perfect control over my permissions. [> Invite](https://discordapp.com/oauth2/authorize/?permissions=2146958839&scope=bot&client_id=481753714982518786)", true)
     .setColor(blue)
-    .addField("Click on the link below", `[SamBot](${botconfig.invitelink})`)
+
   message.channel.send(InviteEmbed)
 }
 
