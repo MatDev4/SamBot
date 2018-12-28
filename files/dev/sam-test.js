4f661575206b448d025a94de26483ad12ec13a22
Ze_Dreams <34602094+Nicoxx45@users.noreply.github.com>
2018-12-28 19:11:42 +0100

    Add files via upload

--- files/dev/sam-test.js
+++ files/dev/sam-test.js
@@ -10,37 +10,7 @@ module.exports.run = async (bot, message, args) => {
   if(message.author.bot) return;
   message.delete()
 
-  const imageUrlRegex = /\?size=2048$/g;
-  const { body: ujavatar } = await get(member.user.displayAvatarURL.replace(imageUrlRegex, "?size=128"));
-  const ujname = member.displayName.length > 20 ? member.displayName.substring(0, 17) + "..." : member.displayName;
-
-const testttt = new Canvas(500, 400)
- .setColor('#f5f0f4')
- .addRect(84, 0, 316, 180)
- .setColor("#2C2F33")
- .addRect(0, 0, 84, 180)
- .addRect(169, 26, 231, 46)
- //.addRect(224, 108, 176, 46)
-    // Shadow effect
- .setShadowColor("rgba(22, 22, 22, 1)") // shadow color.
- .setShadowOffsetY(5) // drop shadow 5 pixels
- .setShadowBlur(10) // Blur shadow by 10.
- .save() // Save
- // Circle 2 pixels radius smaller prevent pixel border
- .addCircle(84, 90, 62)
- .addRoundImage(ujavatar, 20, 26, 128, 128, 64)
- .restore()
- .setTextAlign("center")
- .setTextFont("10pt Discord")
- .setColor("#FFFFFF")
- .addText(ujname, 285, 54)
-
-await message.channel.send({
-  files: [{
-    attachment: testttt,
-    name: `t.jpg`
-  }]
-})
+  message.channel.send("...")
 }
 
 
