34454533b81f6e63f5e571a06660cac843e03506
Ze_Dreams <34602094+Nicoxx45@users.noreply.github.com>
2018-12-28 15:23:27 +0100

    WIP: new unit

---
+++ files/guildConfiguration/see-config.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const fs = require("fs");
+let botconfig = require("../botconfig.json");
+let misc = require("../misc.json");
+let cmdh = require("../cmdh.json");
+const i18n = require('i18n');
+const client = new Discord.Client({disableEveryone: true});
+
+var prefix = (botconfig.prefix)
+
+module.exports.run = async (client, message, args) => {
+  if (message.content.indexOf(prefix) !== 0) return;
+  if (message.channel.type === "dm") return;
+  if(message.author.bot) return;
+  if(!message.author.hasPermission('ADMINISTRATOR')) return message.channel.send("..")
+
+  let f = misc.fleche
+  let ON = misc.DiscordOnline
+  let OFF = misc.DiscordOffline
+
+  const SeeConfigEmbed = new Discord.RichEmbed()
+  .setTitle(`.. ${message.guild.name}`)
+  .setColor(botconfig.discordblue)
+  .setThumbmail(message.guild.iconURL)
+  .addField("#⃣ ..", `.. ${f} ..\n.. ${f} ..\n.. ${f} ..\n.. ${f} ..\n.. ${f} ..`, true)
+  .addField("⚜ ..", `.. ${f} ..\n.. ${f} ..\n.. ${f} ..\n.. ${f} ..\n.. ${f} ..`, true)
+  .addField("🚘 ..", `.. ${f} ..\n.. ${f} ..\n.. ${f} ..\n.. ${f} ..\n.. ${f} ..`, true)
+  message.channel.send(SeeConfigEmbed);
+}
+
+module.exports.help = {
+  name: "see-config"
+}
